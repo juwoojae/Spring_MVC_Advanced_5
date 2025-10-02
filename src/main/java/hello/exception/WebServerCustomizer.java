bcd3316a295e41b8fd5d807bea4c3a55c2aaef0f
Juwoojae <juwqq1234@naver.com>
2025-10-02 20:57:25 +0900

    서블릿 예외 처리 - 오류 페이지 작동 원리

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -4,7 +4,9 @@ import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
@@ -15,3 +17,20 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
         factory.addErrorPages(errorPage404, errorPage500 ,errorPageEx);
     }
 }
+
+/**
+ * 1. 예외 발생 흐름
+ * WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+ * 2. sendError
+ *  WAS(sendError 호출 기록 확인) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러
+ * (response.sendError())
+ * 1.RuntimeException 예외가 WAS 까지 전달되면, WAS 는 오류 페이지 정보를 확인한다. (WebServerCustomizer)
+ *
+ * 1. WAS(여기까지 전파) <- 필터 <- 서블릿 <- 인터셉터 <- 컨트롤러(예외발생)
+ *  2. WAS `/error-page/500` 다시 요청 -> 필터 -> 서블릿 -> 인터셉터 -> 컨트롤러(/error-page/
+ *  500) -> View
+ *
+ * 2. 오류 정보 추가
+ * WAS 는 오류 페이지가 발생하면 페이지를 찾아서 호출함과 동시에 request 에 attribute 에 오류를 추가해서 넘겨존다
+ *
+ */
