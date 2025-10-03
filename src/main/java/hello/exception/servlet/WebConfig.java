750e4b690e9dad3c893c03dfe7689b70fc75988c
Juwoojae <juwqq1234@naver.com>
2025-10-04 02:59:10 +0900

    서블릿 예외 처리 - 인터셉터

--- src/main/java/hello/exception/servlet/WebConfig.java
+++ src/main/java/hello/exception/servlet/WebConfig.java
@@ -1,11 +1,13 @@
 package hello.exception.servlet;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.swing.*;
@@ -13,7 +15,15 @@ import javax.swing.*;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");//오류 페이지 경로
+    }
+
+   // @Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -25,3 +35,10 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 }
+/**
+ * 기본적으로 컨트롤러에서 예외를 발생하면 WAS 까지 전파해준후(예외 전파)
+ * 그리고, WAS 가 해당 오류에 맞는 오류페이지를 재 요청한다.
+ * 이 과정에서 필터, 인터셉터가 한번씩 더 호출될수있어서 , 의도와는 다르게 실행이 될수있는데, 이것을 막는법
+ * 필터는 DispatchType 으로 중복 호출 제거 ( dispatchType=REQUEST )
+ * 인터셉터는 경로 정보로 중복 호출 제거( excludePathPatterns("/error-page/**") )
+ */
